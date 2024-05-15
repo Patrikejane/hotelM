99f85d29b16160f11f73ee21caeef053db45e31c
Sunimal Malkaklulage <sskmal@gmail.com>
2024-05-16 01:17:40 +0900

    Update reservation.module.ts
    Form Modules Reactive Forms

--- src/app/reservation/reservation.module.ts
+++ src/app/reservation/reservation.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReservationComponent } from './reservation.component';
 import { ReservationFormComponent } from '../reservation-form/reservation-form.component';
 import { ReservationListComponent } from '../reservation-list/reservation-list.component';
-
+import { ReservationComponent } from './reservation.component';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     ReservationComponent,
     ReservationFormComponent,
-    ReservationListComponent
+    ReservationListComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class ReservationModule { }
