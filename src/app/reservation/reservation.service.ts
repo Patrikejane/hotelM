3b32eb97667eadeed0048b9f4283b0e02279fc65
Sunimal Malkaklulage <sskmal@gmail.com>
2024-05-16 01:17:53 +0900

    Update reservation.service.ts
    Crud Operations

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../model/reservation';
+
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,34 @@ import { Injectable } from '@angular/core';
 export class ReservationService {
 
   constructor() { }
+
+  private reservations : Reservation[] = [];
+
+
+  //C R U D
+  getReservations(): Reservation [] {
+    return this.reservations;
+  }
+
+  getReservation(id: string): Reservation | undefined {
+    return this.reservations.find(res => res.id === id);
+  }
+
+  addReservation(reservation:Reservation) : void{
+    this.reservations.push(reservation);
+  }
+
+  deleteReservation(id: string): void{
+    let index = this.reservations.findIndex(res => res.id === id); 
+    this.reservations.splice(index,1);
+  }
+
+
+  updateReservation(updatedReservation:Reservation): void {
+    let index = this.reservations.findIndex(res => res.id === updatedReservation.id); 
+    this.reservations[index] = updatedReservation;
+  }
+
+
+
 }
