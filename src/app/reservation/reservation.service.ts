2b9458642c62628f73ec230af03e51aa5cf0ae76
Sunimal Malkaklulage <sskmal@gmail.com>
2024-05-20 04:23:19 +0900

    Update reservation.service.ts
    LocalStorage Added

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -7,7 +7,12 @@ import { Reservation } from '../model/reservation';
 })
 export class ReservationService {
 
-  constructor() { }
+  constructor() {
+
+    let savedReservation = localStorage.getItem("reservations");
+    this.reservations = savedReservation? JSON.parse(savedReservation) : [];
+
+   }
 
   private reservations : Reservation[] = [];
 
@@ -23,21 +28,25 @@ export class ReservationService {
   }
 
   addReservation(reservation:Reservation) : void{
+    reservation.id = Date.now().toString();
     this.reservations.push(reservation);
     console.log(this.reservations);
+    localStorage.setItem("reservations",JSON.stringify(this.reservations));
   }
 
   deleteReservation(id: string): void{
     let index = this.reservations.findIndex(res => res.id === id); 
     this.reservations.splice(index,1);
     console.log(this.reservations);
+    localStorage.setItem("reservations",JSON.stringify(this.reservations));
   }
 
 
-  updateReservation(updatedReservation:Reservation): void {
-    let index = this.reservations.findIndex(res => res.id === updatedReservation.id); 
+  updateReservation(id: string,updatedReservation:Reservation): void {
+    let index = this.reservations.findIndex(res => res.id === id); 
     this.reservations[index] = updatedReservation;
     console.log(this.reservations);
+    localStorage.setItem("reservations",JSON.stringify(this.reservations));
   }
 
 
